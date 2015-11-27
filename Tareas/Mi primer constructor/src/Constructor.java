f8422812afe812b5475dcff29fe8d71416b324e7
Suehellen Méndez <suemendez1602@gmail.com>
2015-11-26 19:49:45 -0500

    Mi primer constructor
    constructor

---
+++ Tareas/Mi primer constructor/src/Constructor.java
@@ -0,0 +1,17 @@
+/** Ejemplo de un constructor 
+ * 
+ */
+
+/**
+ * @author Suehellen Méndez
+ *
+ */
+      public class Constructor {
+           static String nombre;
+            static int edad;
+	
+	public static void imprimir (){
+		System.out.println(nombre+" edad: " +edad );
+	}
+
+}
---
+++ Tareas/Mi primer constructor/src/minombre.java
@@ -0,0 +1,11 @@
+
+public class minombre {
+	
+	public static void main(String [] args){
+		
+		Constructor.nombre="Maria";
+		Constructor.edad=26;
+		Constructor.imprimir();
+	}
+
+}
